f7cbc5427fa8b7a1d010d4d792391e7fc7d067c8
Julian Lees <julian.j.lees@gmail.com>
2017-11-07 12:39:11 +1300

    push for integration with Jack

--- src/main/java/entity/Order_API.java
+++ src/main/java/entity/Order_API.java
@@ -11,12 +11,12 @@ public class Order_API {
     }
 
     // Get full order details from the API (by user_id or staff_id)
-    public static Order getOrderDetailsAPI(int actor_id, String actor) {
-        String api_url = api_base_url + actor + "/" + actor_id;
-
-        try {
-
-        }
-
-    }
+//    public static Order getOrderDetailsAPI(int actor_id, String actor) {
+//        String api_url = api_base_url + actor + "/" + actor_id;
+//
+//        try {
+//
+//        }
+//
+//    }
 }
