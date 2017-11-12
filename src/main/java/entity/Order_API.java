ef5618efed48971b4598c4e30c06f34e70e21061
Julian Lees <julian.j.lees@gmail.com>
2017-11-13 12:00:49 +1300

    Order assignment and completion updated

--- src/main/java/entity/Order_API.java
+++ src/main/java/entity/Order_API.java
@@ -110,6 +110,94 @@ public class Order_API {
     //TODO
     //Method for getting an orderlist by customer id
     public static ArrayList<Order> getOrdersByCustomerID(int customer_id) {
+
+        String api_url = api_base_url + "list/customer/" + customer_id;
+
+        try {
+            //Request the json resource at the specified url
+            URL url = new URL(api_url);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+
+            //Convert the JSON object to access data
+            JsonParser jp = new JsonParser(); //json parser from gson library
+            JsonArray order_json_list = jp.parse(new InputStreamReader((InputStream)request.getContent())).getAsJsonArray();
+
+            System.out.println(order_json_list);
+
+//            JsonArray order_jsonArray = order_json_list.getAsJsonArray();
+            ArrayList<Order> order_list = new ArrayList<Order>();
+
+            for(int n = 0; n < order_json_list.size(); n++) {
+                JsonObject order_json = order_json_list.get(n).getAsJsonObject();
+
+                //Get item details and pass into ingredient objects
+                JsonObject item_details = null;
+                try {
+                    item_details = order_json.get("item_details_list").getAsJsonObject();
+                } catch (IllegalStateException e) {
+                    //Catch case where order does not exist
+                    System.out.println("Order does not exist");
+                    return null;
+                }
+
+                //Get order metadata and parse elements into an order object
+                JsonObject order_meta = order_json.get("order_details_list").getAsJsonObject();
+                //Process order_meta
+                int order_id_received = order_meta.get("Order_ID").getAsInt();
+                Staff staff = null;
+//                int customer_id = order_meta.get("Customer_ID").getAsInt();
+                Customer customer = new Customer(customer_id, null, null, null, -1, null, null, null, null);
+                String datetimeString = order_meta.get("DateTime").getAsString();
+                int order_status_num = order_meta.get("Status").getAsInt();
+                String order_status = Status_Information.getStatus(order_status_num);
+
+
+                //Get items and ingredients
+                ArrayList<Item> items = new ArrayList<>();
+                //For each item, process the ingredients
+                for (Map.Entry<String, JsonElement> entry : item_details.entrySet()) {
+
+                    int item_id = Integer.parseInt(entry.getKey());
+                    //Initialise item ingredients array as a new array
+                    ArrayList<Stock> ingredients = new ArrayList<>();
+
+                    String item_type = "";
+
+                    JsonArray ingredient_array = entry.getValue().getAsJsonArray();
+
+                    for (int i = 0; i < ingredient_array.size(); i++) {
+
+                        int stock_id = Integer.parseInt(ingredient_array.get(i).toString());
+
+                        //Check the first ingredients to confirm the item type
+                        if (i == 0) {
+                            item_type = Stock_Information.getItemType(stock_id);
+                        }
+
+                        String stock_name = Stock_Information.getIngredientName(stock_id);
+                        String category = Stock_Information.getItemCategory(stock_id);
+                        double price = Stock_Information.getIngredientPrice(stock_id);
+                        Stock ingredient = new Stock(stock_id, stock_name, category, -1, price, "");
+
+                        ingredients.add(ingredient);
+                    }
+
+                    Item new_item = new Item(item_id, order_id_received, ingredients, item_type);
+                    items.add(new_item);
+                }
+
+                Order order = new Order(order_id_received, staff, customer, datetimeString, order_status, items);
+                order_list.add(order);
+
+            }
+
+            return order_list;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
@@ -272,6 +360,34 @@ public class Order_API {
 //        Order_API.addOrder(order, 21);
 
 //        Order_API.assignStaffToOrder(1, 21);
-        Order_API.completeOrder(21);
+//        Order_API.completeOrder(21);
+
+        ArrayList<Order> test = Order_API.getOrdersByCustomerID(21);
+
+        for (int i = 0; i < test.size(); i++) {
+
+            Order order = test.get(i);
+            System.out.println("New order: " + order.getStatus());
+
+            for (int j = 0; j < order.getItems().size(); j++) {
+
+                Item item = order.getItems().get(j);
+
+                System.out.println("    Item: " + item.getItem_type());
+
+                for (int k = 0; k < item.getIngredients().size(); k++) {
+
+                    Stock ingredient = item.getIngredients().get(k);
+
+                    System.out.println("        Ingredient: " + ingredient.getIngredient_name() + " " + ingredient.getPrice());
+
+                }
+
+
+            }
+
+        }
+
+//
     }
 }
