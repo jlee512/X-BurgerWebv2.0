8424dab056ae37d487589dd798e21776c98041db
jack <jack_allan1@hotmail.com>
2017-11-13 16:02:43 +1300

    commit order page updates

---
+++ web/js/orderpage.js
@@ -0,0 +1,53 @@
+
+function showCheese () {
+
+	document.getElementById("cheeseCol").style.display = "inline-block";
+	document.getElementById("beefCol").style.display = "none";
+	document.getElementById("saladCol").style.display = "none";
+	document.getElementById("bunCol").style.display = "none";
+	document.getElementById("sauceCol").style.display = "none";
+
+
+}
+
+
+function showBeef () {
+
+	document.getElementById("beefCol").style.display = "inline-block";
+	document.getElementById("cheeseCol").style.display = "none";
+	document.getElementById("saladCol").style.display = "none";
+	document.getElementById("bunCol").style.display = "none";
+	document.getElementById("sauceCol").style.display = "none";
+
+
+}
+
+function showSalad () {
+
+	document.getElementById("saladCol").style.display = "inline-block";
+	document.getElementById("cheeseCol").style.display = "none";
+	document.getElementById("beefCol").style.display = "none";
+	document.getElementById("bunCol").style.display = "none";
+	document.getElementById("sauceCol").style.display = "none";
+
+}
+
+function showBun () {
+
+	document.getElementById("bunCol").style.display = "inline-block";
+	document.getElementById("cheeseCol").style.display = "none";
+	document.getElementById("beefCol").style.display = "none";
+	document.getElementById("saladCol").style.display = "none";
+	document.getElementById("sauceCol").style.display = "none";
+
+}
+
+function showSauce () {
+
+	document.getElementById("sauceCol").style.display = "inline-block";
+	document.getElementById("cheeseCol").style.display = "none";
+	document.getElementById("beefCol").style.display = "none";
+	document.getElementById("saladCol").style.display = "none";
+	document.getElementById("bunCol").style.display = "none";
+
+}
