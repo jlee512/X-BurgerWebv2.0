af73f605ab43b20cd0bb29c16241fc82c90fc665
Julian Lees <julian.j.lees@gmail.com>
2017-11-14 21:27:04 +1300

    Resized images to correct aspect ratio

---
+++ web/js/payment.js
@@ -0,0 +1,10 @@
+/**
+ * Created by julia on 14/11/2017.
+ */
+<div class="col-xs-5 col-md-5 pull-right">
+    <div class="form-group">
+    <label for="cvCode">
+    CCV</label>
+    <input type="password" class="form-control" id="cvCode" name="ccv" placeholder="CV" required />
+</div>
+</div>
